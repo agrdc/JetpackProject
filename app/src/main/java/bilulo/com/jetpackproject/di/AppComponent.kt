c2b991ce694b1269a64f94a202965d1114b5e1d3
Agnaldo Rodrigues de Carvalho <agnaldo.carvalho@cedrotech.com>
2019-02-20 10:29:07 -0300

    Removes unecessary inject methods from AppComponent
    -Removes unecessary annotation targets on ViewModelKey
    
    Signed-off-by: Agnaldo Rodrigues de Carvalho <agnaldo.carvalho@cedrotech.com>

--- app/src/main/java/bilulo/com/jetpackproject/di/AppComponent.kt
+++ app/src/main/java/bilulo/com/jetpackproject/di/AppComponent.kt
@@ -1,9 +1,6 @@
 package bilulo.com.jetpackproject.di
 
-import bilulo.com.jetpackproject.data.api.AlbumsService
-import bilulo.com.jetpackproject.repository.AlbumRepository
 import bilulo.com.jetpackproject.ui.list.ListFragment
-import bilulo.com.jetpackproject.vm.ViewModelFactory
 import dagger.Component
 import javax.inject.Singleton
 
@@ -11,6 +8,4 @@ import javax.inject.Singleton
 @Singleton
 interface AppComponent {
     fun inject (listFragment: ListFragment)
-    fun inject(albumRepository: AlbumRepository)
-    fun inject (albumsService: AlbumsService)
 }
--- app/src/main/java/bilulo/com/jetpackproject/di/ViewModuleKey.kt
+++ app/src/main/java/bilulo/com/jetpackproject/di/ViewModuleKey.kt
@@ -5,11 +5,7 @@ import dagger.MapKey
 import kotlin.reflect.KClass
 
 
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
+@Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 @MapKey
 internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
